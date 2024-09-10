198c3d14b3fb23ed683f7aceb8b34ee87c99612b
SwampPear <michaeljosephvaden03@gmail.com>
2024-09-10 16:23:42 -0400

    set up fiber project

---
+++ routes/routes.go
@@ -0,0 +1,10 @@
+package routes
+
+import (
+    "github.com/gofiber/fiber/v2"
+    "github.com/SwampPear/personal-website/handlers"
+)
+
+func SetupRoutes(app *fiber.App) {
+	app.Get("/", handlers.Index)
+}
