3a4751ae5ef81335eb0d631cf1e98466070b7249
SwampPear <michaeljosephvaden03@gmail.com>
2024-09-11 23:01:39 -0400

    working on getting animation menu to work

--- static/js/index.js
+++ static/js/index.js
@@ -1,14 +1,46 @@
 document.addEventListener('DOMContentLoaded', () => {
     // DOM
     const nav = document.querySelector( '.nav' )
+    const menuButton = document.querySelector( '.nav__menu__button' )
+    const menuWrapper = document.querySelector( '.nav__menu__wrapper' )
+    const navMenuFades = document.querySelectorAll( '.nav__menu__fade' )
 
     // state
-    let navVisible = false;
+    let navVisible = false
+    let menuOpen = false
 
-    document.addEventListener("scroll", (event) => {
+    const onScroll = () => {
         if ( window.scrollY >= ( window.innerHeight * 0.9 ) && !navVisible ) {
             nav.classList.add( 'nav__fade-in' )
             navVisible = true
         }
-    })
+    }
+
+    const onMenuOpen = () => {
+        switch ( menuOpen ) {
+            case true:
+                menuWrapper.classList.remove( 'nav__menu__wrapper__open' )
+                menuWrapper.classList.add( 'nav__menu__wrapper__close' )
+                navMenuFades.forEach( ( element ) => {
+                    element.classList.remove( 'nav__menu__fade-in' )
+                    element.classList.add( 'nav__menu__fade-out' )
+                })
+                menuOpen = false
+
+                break
+            case false:
+                menuWrapper.classList.remove( 'nav__menu__wrapper__close' )
+                menuWrapper.classList.add( 'nav__menu__wrapper__open' )
+                navMenuFades.forEach( ( element ) => {
+                    element.classList.remove( 'nav__menu__fade-out' )
+                    element.classList.add( 'nav__menu__fade-in' )
+                })
+                menuOpen = true
+
+                break
+        }
+    }
+
+    document.addEventListener( 'scroll', onScroll )
+    menuButton.addEventListener( 'click', onMenuOpen )
 })
