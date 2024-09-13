5bf869febfa526316e5ff511843922fb75836bd7
SwampPear <michaeljosephvaden03@gmail.com>
2024-09-13 13:33:23 -0400

    abstracted quotes into own file

--- static/js/index.js
+++ static/js/index.js
@@ -14,16 +14,6 @@ document.addEventListener('DOMContentLoaded', () => {
     // state
     let navVisible = false
     let menuOpen = false
-    let quotes = [
-        {
-            quote: "\“I... a universe of atoms, an atom in the universe.\”",
-            citation: "- Richard P. Feynman"
-        },
-        {
-            quote: "\"Nobody ever figures out what life is all about, and it doesn't matter. Explore the world. Nearly everything is really interesting if you go into it deeply enough.\"",
-            citation: "- Richard P. Feynman"
-        }
-    ]
 
     const randInt = ( max ) => {
         return Math.floor( Math.random() * ( max + 1 ) )
---
+++ static/js/quotes.js
@@ -0,0 +1,10 @@
+let quotes = [
+    {
+        quote: "\“I... a universe of atoms, an atom in the universe.\”",
+        citation: "- Richard P. Feynman"
+    },
+    {
+        quote: "\"Nobody ever figures out what life is all about, and it doesn't matter. Explore the world. Nearly everything is really interesting if you go into it deeply enough.\"",
+        citation: "- Richard P. Feynman"
+    }
+]
