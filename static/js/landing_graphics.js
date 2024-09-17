734ed8aef6c5f93dad5063aaeb72d4c31a3ac7a6
SwampPear <michaeljosephvaden03@gmail.com>
2024-09-16 20:48:26 -0400

    got wasm animation working

--- static/js/landing_graphics.js
+++ static/js/landing_graphics.js
@@ -1,13 +1,38 @@
 document.addEventListener( 'DOMContentLoaded', () => {
-    const landingBackground = document
+    const graphic = document.querySelector( '.landing__graphic' )
+
     async function init() {
         const { instance } = await WebAssembly.instantiateStreaming(
           fetch("/wasm/website_graphics.wasm")
-        )
-    
-        const answer = instance.exports.the_answer();
-        console.log(answer);
+        );
+
+        const width = 600;
+        const height = 600;
+
+        graphic.width = width;
+        graphic.height = height;
+
+        const buffer_address = instance.exports.BUFFER.value;
+        const image = new ImageData(
+            new Uint8ClampedArray(
+                instance.exports.memory.buffer,
+                buffer_address,
+                4 * width * height,
+            ),
+            width,
+        );
+
+        const ctx = graphic.getContext("2d");
+
+        // CHANGES BEGIN HERE
+        const render = () => {
+          instance.exports.go();
+          ctx.putImageData(image, 0, 0);
+          requestAnimationFrame(render);
+        };
+
+        render();
       }
-    
-      init()
+
+      init();
 })
