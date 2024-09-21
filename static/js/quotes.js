d25ffceb6c251a2a20afa1264db15866f5b0c09e
SwampPear <michaeljosephvaden03@gmail.com>
2024-09-21 16:58:11 -0400

    added some quotes

--- static/js/quotes.js
+++ static/js/quotes.js
@@ -9,7 +9,35 @@ let quotes = [
     },
     {
         quote: "\"Don't panic.\"",
-        citation: "Douglas Adams"
+        citation: "- Douglas Adams"
+    },
+    {
+        quote: "\"To live is the rarest thing in the world. Most people exist, that is all.\"",
+        citation: "- Oscar Wilde"
+    },
+    {
+        quote: "\"Silence, I discover, is something you can actually hear.\"",
+        citation: "- Haruki Murakami"
+    },
+    {
+        quote: "\"Everyone you will ever meet knows something you don't.\"",
+        citation: "- Bill Nye"
+    },
+    {
+        quote: "\"If I were not a physicist, I would probably be a musician. I often think in music. I live my daydreams in music. I see my life in terms of music.\"",
+        citation: "- Albert Einstein"
+    },
+    {
+        quote: "\"The reality we can put into words is never reality itself.\"",
+        citation: "- Werner Heisenberg"
+    },
+    {
+        quote: "\"The only way to do great work is to love what you do.\"",
+        citation: "- Steve Jobs"
+    },
+    {
+        quote: "\"\"",
+        citation: ""
     }
 ]
 
@@ -17,3 +45,10 @@ let ex = {
     quote: "\"\"",
     citation: ""
 }
+
+let q = 8
+
+
+
+
+
