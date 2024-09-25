f5bb1f56e11773f4431e22fe235e5d12f0d18501
SwampPear <michaeljosephvaden03@gmail.com>
2024-09-24 23:19:37 -0400

    finished 404 page

--- handlers/app.go
+++ handlers/app.go
@@ -3,7 +3,9 @@ package handlers
 import "github.com/gofiber/fiber/v2"
 
 func Index(c *fiber.Ctx) error {
-	return c.Render("index", fiber.Map{
-		"Title": "Hello, World!",
-	})
+	return c.Render("index", fiber.Map{})
+}
+
+func Error404(c *fiber.Ctx) error {
+	return c.Render("404", fiber.Map{})
 }
