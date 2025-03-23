c59f2e2243a91bb5a7a009b5913c94fba4183134
Michael Vaden <michaelvaden.mjv@gmail.com>
2025-03-23 17:22:44 -0400

    cleaned up and abstracted background graphic code

--- website/src/index.ts
+++ website/src/index.ts
@@ -1,89 +1,17 @@
 import { Loader } from './taffy/router'
 import { setTitle } from './taffy/meta'
+import { VERTEX_SHADER, FRAGMENT_SHADER } from './shaders'
+import { compileShader, createProgram } from './utils'
 
-const vertexShaderSource = `
-attribute vec2 position;
-varying vec2 v_tex_coords;
-void main() {
-    v_tex_coords = position * 0.5 + 0.5;
-    gl_Position = vec4(position, 0.0, 1.0);
-}`
-
-const proceduralShader = `
-precision mediump float;
-varying vec2 v_tex_coords;
-uniform float time;
-uniform vec2 resolution;
-
-#define TAU 6.28318530718
-#define MAX_ITER 5
-
-void main() {
-    vec2 fragCoord = (v_tex_coords - 0.5) * resolution / min(resolution.x, resolution.y);
-
-    float t = time * .5 + 24.0;
-    vec2 p = mod(fragCoord.xy * TAU, TAU) - 250.0;
-    vec2 i = vec2(p);
-    float c = 1.0;
-    float inten = .005;
-
-    for (int n = 0; n < MAX_ITER; n++) {
-        float timeShift = t * (1.0 - (3.5 / float(n + 1)));
-        i = p + vec2(cos(timeShift - i.x) + sin(timeShift + i.y),
-                     sin(timeShift - i.y) + cos(timeShift + i.x));
-        c += 1.0 / length(vec2(p.x / (sin(i.x + timeShift) / inten),
-                               p.y / (cos(i.y + timeShift) / inten)));
-    }
-
-    c /= float(MAX_ITER);
-    c = 1.17 - pow(c, 1.4);
-
-    vec3 bg = vec3(0.05, 0.05, 0.05);
-    vec3 fg = vec3(0.095, 0.095, 0.1375);
-    vec3 color = mix(bg, fg, pow(abs(c), 2.0));
-
-
-    gl_FragColor = vec4(color, 1.0);
-}`
-
-const compileShader = (gl: any, sourceCode: any, shaderType: any) => {
-    const shader = gl.createShader(shaderType)
-    gl.shaderSource(shader, sourceCode)
-    gl.compileShader(shader)
-
-    if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
-        console.error('Shader compilation failed: ', gl.getShaderInfoLog(shader))
-        gl.deleteShader(shader)
-        return null
-    }
-
-    return shader
-}
-
-const createProgram = (gl: any, vertexShader: any, fragmentShader: any) => {
-    const program = gl.createProgram()
-    gl.attachShader(program, vertexShader)
-    gl.attachShader(program, fragmentShader)
-    gl.linkProgram(program)
-
-    if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
-        console.error('Program linking failed: ', gl.getProgramInfoLog(program))
-        gl.deleteProgram(program)
-        return null
-    }
-
-    return program
-}
-
-const indexRoute = () => {
+const renderBackground = () => {
     const canvas = document.querySelector('.background > canvas') as HTMLCanvasElement
     if (!canvas) return console.error('Canvas not found')
 
     const gl = canvas.getContext('webgl', { alpha: true })
     if (!gl) return console.error('WebGL not supported')
 
-    const vertexShader = compileShader(gl, vertexShaderSource, gl.VERTEX_SHADER)
-    const fragmentShader = compileShader(gl, proceduralShader, gl.FRAGMENT_SHADER)
+    const vertexShader = compileShader(gl, VERTEX_SHADER, gl.VERTEX_SHADER)
+    const fragmentShader = compileShader(gl, FRAGMENT_SHADER, gl.FRAGMENT_SHADER)
     const program = createProgram(gl, vertexShader, fragmentShader)
 
     const positionBuffer = gl.createBuffer()
@@ -130,12 +58,15 @@ const indexRoute = () => {
 
 const index = () => {
     console.log('Initializing index page.')
+
     setTitle('Michael Vaden')
-    indexRoute()
+
+    renderBackground()
 }
 
 const about = () => {
     console.log('Initializing about page.')
+    
     setTitle('About')
 }
 
---
+++ website/src/shaders.ts
@@ -0,0 +1,46 @@
+const VERTEX_SHADER = `
+attribute vec2 position;
+varying vec2 v_tex_coords;
+void main() {
+    v_tex_coords = position * 0.5 + 0.5;
+    gl_Position = vec4(position, 0.0, 1.0);
+}`
+
+const FRAGMENT_SHADER = `
+precision mediump float;
+varying vec2 v_tex_coords;
+uniform float time;
+uniform vec2 resolution;
+
+#define TAU 6.28318530718
+#define MAX_ITER 5
+
+void main() {
+    vec2 fragCoord = (v_tex_coords - 0.5) * resolution / min(resolution.x, resolution.y);
+
+    float t = time * .5 + 24.0;
+    vec2 p = mod(fragCoord.xy * TAU, TAU) - 250.0;
+    vec2 i = vec2(p);
+    float c = 1.0;
+    float inten = .005;
+
+    for (int n = 0; n < MAX_ITER; n++) {
+        float timeShift = t * (1.0 - (3.5 / float(n + 1)));
+        i = p + vec2(cos(timeShift - i.x) + sin(timeShift + i.y),
+                     sin(timeShift - i.y) + cos(timeShift + i.x));
+        c += 1.0 / length(vec2(p.x / (sin(i.x + timeShift) / inten),
+                               p.y / (cos(i.y + timeShift) / inten)));
+    }
+
+    c /= float(MAX_ITER);
+    c = 1.17 - pow(c, 1.4);
+
+    vec3 bg = vec3(0.05, 0.05, 0.05);
+    vec3 fg = vec3(0.095, 0.095, 0.1375);
+    vec3 color = mix(bg, fg, pow(abs(c), 2.0));
+
+
+    gl_FragColor = vec4(color, 1.0);
+}`
+
+export { VERTEX_SHADER, FRAGMENT_SHADER }
